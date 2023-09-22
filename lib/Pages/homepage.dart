f65af83b9f4924efc7c4d9db8de02ee7caef211f
Israel-Silvestre <israelvaz2705@gmail.com>
2023-09-21 21:49:21 -0300

    Initial commit

---
+++ lib/Pages/homepage.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0; // Índice da opção selecionada na BottomNavigationBar
+
+  // Páginas associadas às opções da BottomNavigationBar
+  final List<Widget> _pages = [
+    // Página inicial (home)
+    Placeholder(
+      color: Colors.green,
+    ),
+    // Página do calendário
+    Placeholder(
+      color: Colors.green,
+    ),
+    // Página da fruta
+    Placeholder(
+      color: Colors.green,
+    ),
+    // Página de configurações
+    Placeholder(
+      color: Colors.green,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nome do App'),
+        backgroundColor: Colors.green, // Cor de fundo da AppBar
+        iconTheme: IconThemeData(color: Colors.white), // Cor dos ícones da AppBar
+      ),
+      body: _pages[_selectedIndex], // Mostra a página correspondente à opção selecionada
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          canvasColor: Colors.green, // Cor de fundo da BottomNavigationBar
+        ),
+        child: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.white), // Ícone branco
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today, color: Colors.white), // Ícone branco
+              label: 'Calendário',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_florist, color: Colors.white), // Ícone branco
+              label: 'Fruta',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings, color: Colors.white), // Ícone branco
+              label: 'Configurações',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.white, // Cor do item selecionado (ícone)
+          onTap: _onItemTapped, // Callback quando um item é pressionado
+        ),
+      ),
+    );
+  }
+
+  // Função para atualizar a página quando um item da BottomNavigationBar é pressionado
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
