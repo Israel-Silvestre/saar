47ac0f6ede3521216be8e9d623650f144d4b4262
george morais <george.morais@sensedata.com.br>
2023-10-05 19:58:42 -0300

    feat: valida existencia de culturas no preferences e chama request

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:saar/Pages/start.dart';
+import 'package:saar/embrapa_api/models.dart';
+import 'package:saar/embrapa_api/service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +10,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Future<void> verificarEArmazenarLista() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final List<String>? culturasSalvas = prefs.getStringList("culturas");
+
+    if (culturasSalvas == null) {
+      final List<Cultura> novasCulturas = await EmbrapAPI.fetchCulturas();
+
+      final List<Map<String, dynamic>> listaJson = novasCulturas.map((obj) => obj.toJson()).toList();
+      await prefs.setString('culturas', listaJson.toString());
+    }
+  }
 
   // This widget is the root of your application.
   @override
