3c0677c8640c62fe14165b495825069f95a53916
george morais <george.morais@sensedata.com.br>
2024-02-02 22:19:06 -0300

    refactor: cria contexto do app

---
+++ lib/app_context.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'embrapa_api/models.dart';
+import 'embrapa_api/service.dart';
+
+class AppContextData extends ChangeNotifier {
+  late Future<List<Soil>> contextSoils;
+  late Future<List<Crop>> contextCrops;
+  late Future<List<dynamic>> contextAvailableYears;
+
+  AppContextData() {
+    _fetchInitialData();
+  }
+
+  Future<void> _fetchInitialData() async {
+    contextSoils = EmbrapAPI.fetchSolos();
+    contextCrops = EmbrapAPI.fetchCulturas();
+    contextAvailableYears = EmbrapAPI.fetchAnosDisponiveis();
+
+    await Future.wait([contextCrops, contextSoils, contextAvailableYears]);
+
+    notifyListeners();
+  }
+
+}
